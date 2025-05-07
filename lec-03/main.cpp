04888c695941816e2025a33bf4aa2d0a472f4ba7
Rudra805 <rudramishra9949@gmail.com>
2025-05-07 21:29:06 +0530

    notes

---
+++ lec-03/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//using namespace std;
+int main(){
+
+      //variable
+    int number=123;
+    //constant
+    const int num=123;
+
+    std::cout<<"hello world"<<std::endl;          //cout for output and endl for end line 
+    return 0;
+
+
+}
